cda93a6b5bef93f70c3207f58e005761772a57be
marlonchca3 <marlonchca3@gmail.com>
2025-12-24 12:59:36 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tercer_repo',
+    version='0.1',
+    packages=find_packages(),
+    author='marlon',
+    author_email='marlonchca3@gmail.com',
+    url='https://github.com/marlonchca3/tercerRepo',
+)
